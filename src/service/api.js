3cb74fd13e976da7620a886a18e9d646374385dc
abhisekkr98 <abhisekkr98@gmail.com>
2022-04-28 00:04:46 +0530

    updated API url

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:5000";
+const URL = "https://blogapp-api-by-abhisek.herokuapp.com";
 
 export const userSignin = async (data) => {
 	try {
